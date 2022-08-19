7e1da02b0ee3338fae7e4b1c7ea26273ff4e7719
Shirley <illangayo00@gmail.com>
2022-08-19 23:59:57 +0900

    how to read object properties

--- object/1/app.js
+++ object/1/app.js
@@ -15,3 +15,40 @@ let user3 = {
 };
 
 console.log(user3);
+
+// #프로퍼티를 읽고, 추가하고, 삭제하기
+// 일단 읽어내야(지정한 것을 집어낼 줄 알아야) 추가와 삭제 가능
+console.log(user3.name);
+console.log(user3.age);
+
+// adding properties
+
+user3.isAdmin = true;
+user3.gender = 'Male';
+console.log(user3);
+
+// deleting properties
+
+delete user3.gender;
+delete user3.isAdmin;
+console.log(user3);
+
+// 'keys' with multiple words >> wrap around with quotation marks.
+// make sure that they are "strings"
+let user4 = {
+  name: 'Helen',
+  age: 31,
+  'likes birds': true,
+};
+
+// objects declared with const can be updated since they just save
+// the reference of the object
+
+const user5 = {
+  name: 'Lion',
+};
+user5.name = 'Tiger';
+console.log(user5.name);
+
+// keys made of multiple words cannot be read with dot method
+// console.log(user4."likes birds") error : unexpected string
