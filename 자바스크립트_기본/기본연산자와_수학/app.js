0a6b7a4ea7818222af3a681e5600912eb3367e07
Shirley <illangayo00@gmail.com>
2022-09-22 00:20:30 +0900

    거듭제곱 연산자까지

---
+++ 자바스크립트_기본/기본연산자와_수학/app.js
@@ -0,0 +1,21 @@
+/* 단항 연산자 */
+let x = 1;
+x = -1;
+console.log(x);
+
+/* 이항 연산자 */
+let y = 1;
+let z = 3;
+console.log(z - y);
+
+/* 나머지 연산자 % */
+console.log(5 % 2);
+console.log(8 % 3);
+
+/* 거듭제곱 연산자 */
+console.log(4 ** (1 / 2));
+console.log(8 ** (1 / 3));
+
+console.log(2 ** 2);
+console.log(2 ** 3);
+console.log(2 ** 4);
