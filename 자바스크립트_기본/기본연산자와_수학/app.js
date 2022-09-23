0f1668eb37a1f4948778ffdaaf939b05dcff8431
Shirley <illangayo00@gmail.com>
2022-09-23 23:49:51 +0900

    이항 연산자 + 실습

--- 자바스크립트_기본/기본연산자와_수학/app.js
+++ 자바스크립트_기본/기본연산자와_수학/app.js
@@ -19,3 +19,16 @@ console.log(8 ** (1 / 3));
 console.log(2 ** 2);
 console.log(2 ** 3);
 console.log(2 ** 4);
+
+/* 이항 연산자 '+'와 문자열 연결 */
+let s = 'my' + 'string';
+console.log(s); // mystring
+
+console.log('1' + 2);
+console.log(2 + '1');
+console.log(2 + 2 + '1');
+// 2 + 2까지는 아직 문자열을 만나기 전이므로 수학연산. 문자열을 만나는 시점에 이전의 숫자가 문자열로 변환됨
+
+/* '+' 외의 다른 산술 연산자가 문자형 피연산자를 다루는 모습 ; 전부 숫자형으로 변함 */
+console.log(6 - '2'); // 4 숫자형 결과
+console.log('6' / '2'); // 3
