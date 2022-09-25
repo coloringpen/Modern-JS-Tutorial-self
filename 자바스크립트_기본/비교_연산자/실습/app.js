9fb2678e42594ede0edf5dd54dbf970648eb01bb
Shirley <illangayo00@gmail.com>
2022-09-25 23:54:36 +0900

    비교연산자 실습 불린형 반환과 문자열 비교

---
+++ 자바스크립트_기본/비교_연산자/실습/app.js
@@ -0,0 +1,13 @@
+/** 불린형 반환 */
+console.log(2 > 1);
+console.log(2 == 1);
+console.log(2 != 1);
+
+/* 반환된 불린값을 변수에 할당하기 */
+let result = 5 > 4;
+console.log(result);
+
+/**문자열 비교 */
+console.log('Z' > 'A');
+console.log('Glow' > 'Glee');
+console.log('Bee' > 'Be');
