fe797c34fe2361f7e622b09f31523915033e5f0f
Shirley <illangayo00@gmail.com>
2022-09-26 00:02:05 +0900

    다른형 같의 비교

--- 자바스크립트_기본/비교_연산자/실습/app.js
+++ 자바스크립트_기본/비교_연산자/실습/app.js
@@ -11,3 +11,19 @@ console.log(result);
 console.log('Z' > 'A');
 console.log('Glow' > 'Glee');
 console.log('Bee' > 'Be');
+
+/**다른 형을 가진 값 간의 비교 */
+console.log('2' > 1);
+console.log('01' == 1); // true
+console.log(true == 1);
+console.log(false == 0);
+
+/* 동등비교시 true반환. 하지만 논리평가 시, 하나는 true, 하나는 false 반환 */
+let hmm = 0;
+console.log(Boolean(hmm));
+
+let hmm2 = '0';
+console.log(Boolean(hmm2));
+
+console.log(hmm == hmm2);
+// 변환 기준이 다른 것들을 잘 구별할 것!
