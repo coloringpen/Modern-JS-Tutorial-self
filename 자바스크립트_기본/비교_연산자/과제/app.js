30278dacb179b4214be0c6e9e08305369ad13c9a
Shirley <illangayo00@gmail.com>
2022-09-26 12:01:42 +0900

    과제 1번 문제

---
+++ 자바스크립트_기본/비교_연산자/과제/app.js
@@ -0,0 +1,8 @@
+/* 결과 예측 */
+console.log(5 > 4); // true
+console.log('apple' > 'pineapple'); //false
+console.log('2' > '12'); // true 여기도 문자열로서 비교해야함!!
+console.log(undefined == null); // true
+console.log(undefined === null); // false
+console.log(null == '\n0\n'); // false null은 항상 undefined하고만 같다!!
+console.log(null === +'\n0\n'); // false 형이 다르다!
