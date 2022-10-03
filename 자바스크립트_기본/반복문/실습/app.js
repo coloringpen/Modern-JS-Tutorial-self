138d86a21760d442fa56ed4edede0e96dc462161
Shirley <illangayo00@gmail.com>
2022-10-03 23:44:06 +0900

    while 반복문 실습

---
+++ 자바스크립트_기본/반복문/실습/app.js
@@ -0,0 +1,18 @@
+/** 'while' 반복문 */
+let i = 0;
+while (i < 3) {
+  console.log(i);
+  i++;
+}
+
+let i2 = 3;
+while (i2 != 0) {
+  console.log(i2);
+  i2--;
+}
+
+let i3 = 3;
+while (i3) {
+  console.log(i3);
+  i3--;
+}
