10ce70860d239d0bc7287cf8fa0e408392d8f3e6
Shirley <illangayo00@gmail.com>
2022-10-06 00:03:48 +0900

    지역변수

--- 자바스크립트_기본/함수/app.js
+++ 자바스크립트_기본/함수/app.js
@@ -3,5 +3,14 @@ function showMessage() {
   alert('Hello!');
 }
 
-showMessage();
-showMessage();
+// showMessage();
+// showMessage(); 같은 동작을 두 번 할 때도, 코드중복 없이 호출만 두 번 하면됨
+
+/** 지역변수 */
+function sayHello() {
+  let message = 'hello!'; // 지역변수
+  console.log(message);
+}
+
+sayHello();
+// console.log(message); // app.js:16 Uncaught ReferenceError: message is not defined
