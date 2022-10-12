425e389fda7f881675f0fa82a87cb7b9fb09b318
Shirley <illangayo00@gmail.com>
2022-10-13 00:55:33 +0900

    함수를 변수에 할당해보기

--- 자바스크립트_기본/함수_표현식/실습/app.js
+++ 자바스크립트_기본/함수_표현식/실습/app.js
@@ -12,5 +12,15 @@ sayHi();
 sayHi2();
 
 /** 함수는 값 -- 값을 출력하는 방식으로 함수 코드를 출력 가능 */
-console.log(sayHi);
-console.log(sayHi2);
+console.log(sayHi); // sayHi() {console.log('hello');}
+console.log(sayHi2); // () {console.log('hello');} 표현식으로 만들어진 함수의 경우, 함수 이름이 출력되진 않음
+
+/* 함수를 복사해 다른 변수에 할당도 가능 */
+let func = sayHi;
+let func1 = sayHi2;
+
+console.log(func1); // () {console.log('hello');}
+console.log(func); // sayHi() {console.log('hello');}
+func();
+func1;
+sayHi();
