a0fa2649de82294108f046c9a97cf676650e9fe7
Shirley <illangayo00@gmail.com>
2022-10-19 23:54:25 +0900

    콜백함수 완료

--- 자바스크립트_기본/함수_표현식/실습/app.js
+++ 자바스크립트_기본/함수_표현식/실습/app.js
@@ -120,3 +120,42 @@ welcome1();
 welcome2();
 
 /** 콜백함수 */
+// function ask(question, yes, no) {
+//   if (confirm(question)) yes();
+//   else no();
+// }
+
+// function showOk() {
+//   console.log('동의하셨습니다');
+// }
+
+// function showCancel() {
+//   console.log('취소 버튼을 누르셨습니다');
+// }
+
+// ask('동의하십니까?', showOk, showCancel);
+
+function ask(question, yes, no) {
+  if (confirm(question)) yes();
+  else no();
+}
+
+// ask(
+//   '동의하십니까?',
+//   () => {
+//     console.log('동의하셨습니다');
+//   },
+//   () => {
+//     console.log('취소버튼을 누르셨습니다');
+//   }
+// );
+
+// ask(
+//   '동의하십니까?',
+//   function () {
+//     console.log('동의하셨습니다');
+//   },
+//   function () {
+//     console.log('취소버튼을 누르셨습니다');
+//   }
+// );
