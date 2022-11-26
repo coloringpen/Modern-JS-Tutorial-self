acc5f21b1c06991a7c726bb9dda695b91fbdb8ec
Shirley <illangayo00@gmail.com>
2022-11-27 00:13:41 +0900

    new.target 프로퍼트 코멘트

--- 객체_기본/new_생성자함수/실습/app.js
+++ 객체_기본/new_생성자함수/실습/app.js
@@ -1,3 +1,5 @@
+'use strict';
+
 /** 생성자 함수 */
 function User(name) {
   this.name = name;
@@ -18,12 +20,15 @@ let user1 = {
 console.log(user1.name);
 console.log(user1.isAdmin);
 
-/** new.target과 생성자함수 */
+/** new.target과 생성자함수 - new.target은 new없이 호출되면 undefined, 아니면 함수 자체 반환*/
 function Usertwo(name) {
   if (!new.target) {
     return new Usertwo(name);
   }
   this.name = name;
+  console.log(new.target); // 어짜피 new를 붙인것처럼 호출되므로 함수 반환함
+  // 위의 if문이 없다면, 아래 bora에 저장되는 객체 없음. new없이 호출되므로
+  // 그리고 console.log(new.target)도 undefined 반환
 }
 
 let bora = Usertwo('bora');
