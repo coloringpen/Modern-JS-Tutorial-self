c6945fc41f942e53ef13ee2e18bc4622612dd25f
Shirley <illangayo00@gmail.com>
2023-02-16 23:54:30 +0900

    잠시 타입 정리

--- 함수_심화/오래된VAR/실습/app.js
+++ 함수_심화/오래된VAR/실습/app.js
@@ -13,3 +13,38 @@ if (true) {
 }
 
 // console.log(test2); app.js:15 Uncaught ReferenceError: test2 is not defined
+
+const animals = ['ant', 'bison', 'camel', 'duck', 'elephant'];
+
+// 인자와 리턴값 모두 generic
+function last(arr) {
+  return arr[-1];
+}
+
+// 인자와 리턴값 모두 generic
+function prepend(arr, item) {
+  return [item, ...arr];
+}
+
+// 인자와 리턴값 모두 generic
+function mix(arr1, arr2) {
+  return [...arr1, ...arr2];
+}
+
+// 인자는 generic 배열, 리턴값은 number
+function count(arr) {
+  return arr.length;
+}
+
+// generic 배열 인자, number나 null 리턴값
+function findIndex(arr, item) {
+  const result = arr.findIndex((i) => i === item);
+  return result === -1 ? null : result;
+}
+
+// generic배열, number 인자 두개, generic 리턴값
+function slice(arr, startIndex, endIndex) {
+  return arr.slice(startIndex, endIndex + 1);
+}
+
+console.log(count(animals));
